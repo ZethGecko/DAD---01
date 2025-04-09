65225046752b299de79a26a6fc944dee8c810f20
ZethGecko <nelsonluque098@gmail.com>
2025-04-09 18:48:02 -0500

    configuracion

--- ms-historial-service/src/main/java/com/example/mshistorialservice/entity/Historial.java
+++ ms-historial-service/src/main/java/com/example/mshistorialservice/entity/Historial.java
@@ -1,4 +1,26 @@
 package com.example.mshistorialservice.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class Historial {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false) // Clave foránea hacia Usuario
+    private Usuario usuario;
+
+    private Integer montoPago;
+
+    @ManyToOne
+    @JoinColumn(name = "producto_id", nullable = false) // Clave foránea hacia Producto
+    private Producto producto;
+
+    private String accion; // Ejemplo: Describir la acción realizada
+
+    private LocalDateTime fechaAccion;
 }
