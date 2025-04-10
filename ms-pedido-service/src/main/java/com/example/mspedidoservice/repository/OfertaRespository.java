f3805e14a2844fe9cfd9f57001a4e4fe4a0b6178
ZethGecko <nelsonluque098@gmail.com>
2025-04-09 23:09:06 -0500

    configuracion

--- ms-pedido-service/src/main/java/com/example/mspedidoservice/repository/OfertaRespository.java
+++ ms-pedido-service/src/main/java/com/example/mspedidoservice/repository/OfertaRespository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface OfertaRespository extends JpaRepository<Oferta, Integer> {
-    Optional<Object> findByNombre(String nombre);
+    Optional<Object> findBytitulo(String titulo);
 }
